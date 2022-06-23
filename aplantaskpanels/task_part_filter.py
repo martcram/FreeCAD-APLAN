3998de215d5f9bfccd862716ec25f8b3f41c473b
Martijn <martijn.cramer@hotmail.com>
2022-06-23 11:50:49 +0200

    add logic to part filter's task panel

--- aplantaskpanels/task_part_filter.py
+++ aplantaskpanels/task_part_filter.py
@@ -34,6 +34,8 @@ from aplantools import aplanutils
 try:
     import FreeCAD
     import FreeCADGui
+    import itertools
+    import ObjectsAplan
     from PySide2 import QtCore, QtGui, QtWidgets
     import typing
 except ImportError as ie:
@@ -66,6 +68,27 @@ class _TaskPanel:
         self.form = [self._excludePartsForm, self._groupPartsForm]
         self._model: QtGui.QStandardItemModel = QtGui.QStandardItemModel()
 
+        self._parts: typing.Dict = {partObject.Label: partObject
+                                    for partObject in FreeCAD.ActiveDocument.findObjects("Part::Feature")
+                                    if not partObject.isDerivedFrom("Aplan::Compound")}
+        self._partTypes: typing.Dict = {partLabel: self.__getPartType(partLabel) 
+                                        for partLabel in self._parts.keys()}
+        self._initialPartViews: typing.Dict = {label: {"ShapeColor":   partObject.ViewObject.ShapeColor,
+                                                       "Transparency": partObject.ViewObject.Transparency}
+                                               for label, partObject in self._parts.items()}
+
+        self._compounds: typing.Dict = {compound.Label: list(compound.Links)
+                                        for compound in self._obj.Compounds}     
+        self._excludedParts: typing.Dict = {partObject.Label: partObject for partObject in self._obj.ExcludedParts}
+
+        excludedPartLabels_: typing.Set[str] = set(self._excludedParts.keys())
+        groupedPartLabels_: typing.Set[str] = self.__getGroupedPartLabels()
+        self.__initExcludePartsForm()
+        self.__initGroupPartsForm()
+        self.__highlightExcludedParts(excludedPartLabels_)
+        self.__highlightGroupedParts(groupedPartLabels_)
+        self.__unhighlightParts(set(self._parts.keys()).difference(excludedPartLabels_).difference(groupedPartLabels_))
+
         # Connect Signals and Slots
         self._model.itemChanged.connect(self.__partClicked)
         QtCore.QObject.connect(
@@ -85,7 +108,24 @@ class _TaskPanel:
         )
         return button_value
 
-    def accept(self) -> bool:       
+    def accept(self) -> bool:
+        compounds_: typing.Set = set(itertools.chain.from_iterable([compoundGroup.Group for compoundGroup in self._analysis.CompoundGroupObjects]))
+        compoundLabels_: typing.Set = {compound.Label for compound in compounds_}
+        for compoundLabel_ in compoundLabels_:
+            if compoundLabel_ not in self._compounds.keys():
+                FreeCAD.ActiveDocument.removeObject(compoundLabel_)
+        compoundObjects: typing.List = []
+        for compoundLabel, components in self._compounds.items():
+            if compoundLabel not in compoundLabels_:
+                compoundObj = ObjectsAplan.makeCompound(self._analysis, components, compoundLabel)
+                compoundObjects.append(compoundObj)
+        self._obj.Compounds += compoundObjects
+        for compoundGroupObject in self._analysis.CompoundGroupObjects:
+            if len(compoundGroupObject.CompoundObjects) == 0:
+                FreeCAD.ActiveDocument.removeObject(compoundGroupObject.Label)
+        
+        self._obj.ExcludedParts = self._excludedParts.values()
+
         self.__exit()
         return True
 
@@ -94,14 +134,183 @@ class _TaskPanel:
         return True
 
     def __addPartGroup(self) -> None:
-        pass
+        compoundLabel: str = self._analysis.getUniqueObjectLabel("Compound", list(self._compounds.keys()))
+        compoundItem: QtWidgets.QTreeWidgetItem = QtWidgets.QTreeWidgetItem([compoundLabel])
+        partObjects: typing.List[str] = []
+
+        selectedParts_: typing.List[QtWidgets.QTableWidgetItem] = self._groupPartsForm.tw_part_list.selectedItems()
+        if len(selectedParts_) == 0:
+            aplanutils.displayAplanError(message="Unable to create compound!", 
+                                         error="A compound cannot be empty. Please, add some parts before continuing.")
+        elif len(selectedParts_) == 1:
+            aplanutils.displayAplanError(message="Unable to create compound!", 
+                                         error="A compound cannot consist of only one part. Please, add some additional parts before continuing.")
+        else:
+            selectedPart_: QtWidgets.QTableWidgetItem
+            for selectedPart_ in selectedParts_:
+                row: int = selectedPart_.row()
+                partLabel: str = self._groupPartsForm.tw_part_list.item(row, 0).text()
+                partObjects.append(self._parts[partLabel])
+                partItem: QtWidgets.QTreeWidgetItem = QtWidgets.QTreeWidgetItem([partLabel])
+                compoundItem.addChild(partItem)
+                self.__disableItemExcludePartsForm(partLabel)
+                self.__disableItemGroupPartsForm(partLabel)
+                self.__highlightGroupedParts({partLabel})
+            self._compounds[compoundLabel] = partObjects
+            self._groupPartsForm.tw_group_list.insertTopLevelItem(0, compoundItem)
+
+    def __disableItemExcludePartsForm(self, label: str) -> None:
+        items: typing.List[QtGui.QStandardItem] = self._model.findItems(label)
+        item: QtGui.QStandardItem
+        for item in items:
+            item.setEnabled(False)
+            item.setCheckState(QtCore.Qt.Unchecked)
+
+    def __disableItemGroupPartsForm(self, label: str) -> None:
+        items: typing.List[QtWidgets.QTableWidgetItem] = self._groupPartsForm.tw_part_list.findItems(label, QtCore.Qt.MatchFixedString | QtCore.Qt.MatchCaseSensitive)
+        item: QtWidgets.QTableWidgetItem
+        for item in items:
+            row: int = item.row()
+            column: int
+            for column in range(self._groupPartsForm.tw_part_list.columnCount()):
+                item_: QtWidgets.QTableWidgetItem = self._groupPartsForm.tw_part_list.item(row, column)
+                item_.setFlags(item_.flags() & ~QtCore.Qt.ItemIsSelectable)
+                item_.setForeground(QtGui.QBrush(QtGui.QColor("lightgray")))
+
+    def __enableItemExcludePartsForm(self, label: str) -> None:
+        items: typing.List[QtGui.QStandardItem] = self._model.findItems(label)
+        item: QtGui.QStandardItem
+        for item in items:
+            item.setEnabled(True)
+            item.setCheckState(QtCore.Qt.Unchecked)
+
+    def __enableItemGroupPartsForm(self, label: str) -> None:
+        items: typing.List[QtWidgets.QTableWidgetItem] = self._groupPartsForm.tw_part_list.findItems(label, QtCore.Qt.MatchFixedString | QtCore.Qt.MatchCaseSensitive)
+        item: QtWidgets.QTableWidgetItem
+        for item in items:
+            row: int = item.row()
+            column: int
+            for column in range(self._groupPartsForm.tw_part_list.columnCount()):
+                item_: QtWidgets.QTableWidgetItem = self._groupPartsForm.tw_part_list.item(row, column)
+                item_.setFlags(item_.flags() | QtCore.Qt.ItemIsSelectable)
+                item_.setForeground(QtGui.QBrush(QtGui.QColor("black")))
 
     def __exit(self) -> None:
+        if len(self._obj.ExcludedParts) == 0 and len(self._obj.Compounds) == 0:
+            FreeCAD.ActiveDocument.removeObject(self._obj.Name)
+
+        self.__resetPartViews()
         FreeCADGui.ActiveDocument.resetEdit()
         FreeCAD.ActiveDocument.recompute()
 
-    def __partClicked(self, item: QtGui.QStandardItem) -> None:
-        pass
+    def __getGroupedPartLabels(self) -> typing.Set[str]:
+        groupedParts_: typing.Set = set(itertools.chain.from_iterable(self._compounds.values()))
+        return {partObject.Label for partObject in groupedParts_}
 
+    def __getPartType(self, partLabel: str) -> str:
+        subshapesCount: int = len(self._parts[partLabel].Shape.SubShapes)
+        if subshapesCount == 1:
+            return "Part"
+        elif subshapesCount > 1:
+            return "Assembly"
+        else:
+            return "None"
+
+    def __highlightExcludedParts(self, partLabels: typing.Set[str]) -> None:
+        partLabel: str
+        for partLabel in partLabels:
+            viewObject = self._parts[partLabel].ViewObject
+            viewObject.ShapeColor = _TaskPanel.highlightColorExcluded
+            viewObject.Transparency = _TaskPanel.transparencyLevelExcluded
+
+    def __highlightGroupedParts(self, partLabels: typing.Set[str]) -> None:
+        partLabel: str
+        for partLabel in partLabels:
+            viewObject = self._parts[partLabel].ViewObject
+            viewObject.ShapeColor = _TaskPanel.highlightColorGrouped
+            viewObject.Transparency = _TaskPanel.transparencyLevelGrouped
+
+    def __initExcludePartsForm(self) -> None:
+        excludedPartLabels_: typing.Set[str] = set(self._excludedParts.keys())
+        groupedPartLabels_: typing.Set[str] = self.__getGroupedPartLabels()
+        partLabel: str
+        for partLabel in self._parts.keys():
+            item = QtGui.QStandardItem(partLabel)
+            item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
+            if partLabel in groupedPartLabels_:
+                item.setEnabled(False)
+                item.setCheckable(True)
+                item.setCheckState(QtCore.Qt.Unchecked)
+                self.__disableItemExcludePartsForm(partLabel)
+            else:
+                item.setEnabled(True)
+                item.setCheckable(True)
+                item.setCheckState(QtCore.Qt.Checked if partLabel in excludedPartLabels_ else QtCore.Qt.Unchecked)
+            self._model.appendRow(item)
+        self._model.sort(QtCore.Qt.AscendingOrder)
+        self._excludePartsForm.lv_part_list.setModel(self._model)
+
+    def __initGroupPartsForm(self) -> None:
+        groupedPartLabels: typing.Set[str] = self.__getGroupedPartLabels()
+        self._groupPartsForm.tw_part_list.clearContents()
+        self._groupPartsForm.tw_part_list.setRowCount(len(self._parts.keys()))
+        for index, partLabel in enumerate(self._parts.keys()):
+            partItem_: QtWidgets.QTableWidgetItem = QtWidgets.QTableWidgetItem(partLabel)
+            partItem_.setFlags(partItem_.flags() & ~QtCore.Qt.ItemIsEditable)
+            self._groupPartsForm.tw_part_list.setItem(index, 0, partItem_)
+            partTypeItem_: QtWidgets.QTableWidgetItem = QtWidgets.QTableWidgetItem(self._partTypes[partLabel])
+            partTypeItem_.setFlags(partTypeItem_.flags() & ~QtCore.Qt.ItemIsEditable)
+            self._groupPartsForm.tw_part_list.setItem(index, 1, partTypeItem_)
+            if partLabel in groupedPartLabels or partLabel in self._excludedParts.keys():
+                self.__disableItemGroupPartsForm(partLabel)
+        self._groupPartsForm.tw_part_list.sortItems(0, order=QtCore.Qt.AscendingOrder)
+
+        compoundItems: typing.List[QtWidgets.QTreeWidgetItem] = []
+        for compoundLabel, parts in self._compounds.items():
+            partItems: typing.List[QtWidgets.QTreeWidgetItem] = [QtWidgets.QTreeWidgetItem([partObject.Label]) for partObject in parts]
+            compoundItem: QtWidgets.QTreeWidgetItem = QtWidgets.QTreeWidgetItem([compoundLabel])
+            compoundItem.addChildren(partItems)
+            compoundItems.append(compoundItem)
+        self._groupPartsForm.tw_group_list.insertTopLevelItems(0, compoundItems)
+
+    def __partClicked(self, item: QtGui.QStandardItem) -> None:
+        partLabel: str = item.text()
+        partObject = self._parts[partLabel]
+        if item.checkState() == QtCore.Qt.Checked:
+            self.__disableItemGroupPartsForm(partLabel)
+            self._excludedParts[partLabel] = partObject
+            self.__highlightExcludedParts({partLabel})
+        else:
+            self.__enableItemGroupPartsForm(partLabel)
+            self._excludedParts.pop(partLabel, None)
+            self.__unhighlightParts({partLabel})
+    
     def __removePartGroup(self) -> None:
-        pass
+        selectedGroups_: typing.List[QtWidgets.QTreeWidgetItem] = self._groupPartsForm.tw_group_list.selectedItems()
+        selectedGroup_: QtWidgets.QTreeWidgetItem
+        for selectedGroup_ in selectedGroups_:
+            parentItem_: QtWidgets.QTreeWidgetItem = selectedGroup_
+            if selectedGroup_.childCount() == 0:
+                parentItem_ = selectedGroup_.parent()
+            self._groupPartsForm.tw_group_list.takeTopLevelItem(self._groupPartsForm.tw_group_list.indexOfTopLevelItem(parentItem_))
+            del self._compounds[parentItem_.text(0)]
+            childIndex_: int
+            for childIndex_ in range(parentItem_.childCount()):
+                childItemLabel_: str = parentItem_.child(childIndex_).text(0)
+                self.__enableItemGroupPartsForm(childItemLabel_)
+                self.__enableItemExcludePartsForm(childItemLabel_)
+                self.__unhighlightParts({childItemLabel_})
+
+    def __resetPartViews(self):
+        partLabel: str
+        for partLabel, partObject in self._parts.items():
+            viewObject = partObject.ViewObject
+            viewObject.ShapeColor = self._initialPartViews[partLabel]["ShapeColor"]
+            viewObject.Transparency = self._initialPartViews[partLabel]["Transparency"]
+
+    def __unhighlightParts(self, partLabels: typing.Set[str]) -> None:
+        partLabel: str
+        for partLabel in partLabels:
+            viewObject = self._parts[partLabel].ViewObject
+            viewObject.ShapeColor = _TaskPanel.unhighlightColor
+            viewObject.Transparency = _TaskPanel.unhighlightTransparencyLevel
