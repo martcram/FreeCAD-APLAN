6595a2611bf7703f970e90f7bf4d7b92eec34272
martcram <martijn.cramer@hotmail.com>
2022-07-13 11:30:33 +0200

    extend web app API for obstruction graphs

--- aplanwebapp/api.py
+++ aplanwebapp/api.py
@@ -26,15 +26,20 @@ __title__ = "FreeCAD APLAN Flask web app API"
 __author__ = "Martijn Cramer"
 __url__ = "https://www.freecadweb.org"
 
-
-from aplantools import aplanutils
 try:
     import requests
     import typing
 except ImportError as ie:
-    aplanutils.missingPythonModule(str(ie.name or ""))
+    print("Missing dependency! Please install the following Python module: {}".format(str(ie.name or "")))
 
 
+# ********************* General *********************
+def toggleAnimations(enable: bool) -> None:
+    requests.post(
+        "http://0.0.0.0:8080/aplan/animations?enable={}".format(str(enable)))
+
+
+# ********************* Connection graph *********************
 def clearCacheConnectionGraph() -> None:
     requests.post("http://0.0.0.0:8080/aplan/connection_graph/clear_cache")
 
@@ -48,7 +53,7 @@ def getConnectionGraph() -> typing.Dict:
 def getConnectionGraphEdges() -> typing.Set[typing.Tuple]:
     connectionGraph: typing.Dict = getConnectionGraph()
     if connectionGraph:
-        return {tuple(sorted([link["source"], link["target"]])) for link in getConnectionGraph()["links"]}
+        return {tuple(sorted([link["source"], link["target"]])) for link in connectionGraph["links"]}
     else:
         return set()
 
@@ -59,6 +64,26 @@ def getSelectedConnections() -> typing.Dict:
     return r.json()
 
 
-def toggleAnimations(enable: bool) -> None:
-    requests.post(
-        "http://0.0.0.0:8080/aplan/animations?enable={}".format(str(enable)))
+# ********************* Obstruction graph *********************
+def clearCacheObstructionGraph() -> None:
+    requests.post("http://0.0.0.0:8080/aplan/obstruction_graph/clear_cache")
+
+
+def getObstructionGraph() -> typing.Dict:
+    r = requests.get(
+        "http://0.0.0.0:8080/aplan/obstruction_graph/json")
+    return r.json()
+
+
+def getObstructionGraphEdges() -> typing.Set[typing.Tuple]:
+    obstructionGraph: typing.Dict = getObstructionGraph()
+    if obstructionGraph:
+        return {tuple(sorted([link["source"], link["target"]])) for link in obstructionGraph["links"]}
+    else:
+        return set()
+
+
+def getSelectedObstructions() -> typing.Dict:
+    r = requests.get(
+        "http://0.0.0.0:8080/aplan/obstruction_graph/selected_obstructions")
+    return r.json()
